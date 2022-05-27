c7b90687928150162abbfb6abe7a9c2dd06819ae
Ahmet Yigit <ahmet_yigit_02@hotmail.com>
2022-05-27 22:48:10 +0300

    Main Page Added

--- banksystem/banksystem/settings.py
+++ banksystem/banksystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)vuwf@r3mbsk)x@)%gpmm9z&yfipq^%-u1aav7y#e(6=dk(r-g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
